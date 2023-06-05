a43a34f955c47501dcbf67da32a64fad3606bed5
RC-Asus <sharon.code@proton.me>
2023-06-05 23:48:07 +0200

    resize recipes images

--- scripts/factories/recipe.js
+++ scripts/factories/recipe.js
@@ -1,6 +1,15 @@
+// DOM Elements
+const recipesContainer_elt = document.querySelector('.recipesContainer');
+
 export function getRecipe(recipes){
     for(const recipe in recipes){
         const {id, image, name, servings, ingredients, time, description, appliance, ustensils} = recipes[recipe];
-        
+
+        let picture = `./assets/images/mini/${image}`;
+        const img = document.createElement('img');
+        img.src = picture;
+        img.alt = name;
+        recipesContainer_elt.appendChild(img)
+
     }
 }
